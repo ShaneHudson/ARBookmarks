7db87ecd92fa1ad061946bf42d410472ee799320
Shane Hudson <shane@shanehudson.net>
2018-09-14 10:10:58 +0100

    Release v0.2
    This update adds the ability to specify URL and see which URL has been clicked on.
    
    - When you add a link, it now gives a textbox to write the URL.
    - When you click on an existing link it shows the URL and gives option to cancel or open in Safari.

--- ARBookmarks/Scene.swift
+++ ARBookmarks/Scene.swift
@@ -11,7 +11,7 @@ import ARKit
 
 class Scene: SKScene {
     var sight: SKSpriteNode!
-
+    
     override func didMove(to view: SKView) {
         sight = SKSpriteNode(imageNamed: "sight")
         addChild(sight)
@@ -21,40 +21,71 @@ class Scene: SKScene {
         // Called before each frame is rendered
     }
     
+    
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let location = sight.position
+        
+        // Get all objects hit by touch, ignore the first as that is the crosshair
+        let targets = nodes(at: location).dropFirst()
+        
+        if (targets.count > 0) {
+            let target = targets.first as! SKLabelNode
+            let url = target.name!
+            promptOpenURL(url: url)
+        } else {
+            promptForURL()
+        }
+    }
+    
+    func promptOpenURL(url: String) {
+        print("Input: Opening URL " + url)
+        let ac = UIAlertController(title: url, message: "Do you want to open this URL in Safari?", preferredStyle: .alert)
+        
+        let submitAction = UIAlertAction(title: "Yes", style: .default) { (action) -> Void in
+            UIApplication.shared.open(URL(string: url)!, options: [:])
+        }
+
+        let cancelAction = UIAlertAction(title: "No", style: .cancel) { (action) -> Void in
+            print("Input: Opening URL cancelled")
+        }
+
+        ac.addAction(submitAction)
+        ac.addAction(cancelAction)
+        view?.window?.rootViewController?.present(ac, animated: true)
+    }
+    
+    func promptForURL() {
+        let ac = UIAlertController(title: "Enter URL", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned ac] _ in
+            let answer = ac.textFields![0]
+            print("Input: Adding URL " + answer.text!)
+            self.addAnchor(url: URL(string: answer.text!)!)
+        }
+        
+        ac.addAction(submitAction)
+        view?.window?.rootViewController?.present(ac, animated: true, completion: nil)
+    }
+    
+    func addAnchor(url: URL) {
         guard let sceneView = self.view as? ARSKView else {
             return
         }
-        
+
         // Create anchor using the camera's current position
         if let currentFrame = sceneView.session.currentFrame {
-            
-            let location = sight.position
-            let targets = nodes(at: location).dropFirst()
-                
-            if (targets.count > 0) {
-                let target = targets.first as! SKLabelNode
-                let url = target.name!
-                print("opening url " + url)
-                UIApplication.shared.open(URL(string: url)!, options: [:])
-            } else {
-                // Create a transform with a translation of 0.2 meters in front of the camera
-                var translation = matrix_identity_float4x4
-                translation.columns.3.z = -0.8
-                let transform = simd_mul(currentFrame.camera.transform, translation)
+            // Create a transform with a translation of 0.2 meters in front of the camera
+            var translation = matrix_identity_float4x4
+            translation.columns.3.z = -0.8
+            let transform = simd_mul(currentFrame.camera.transform, translation)
 
 
-                // debug code to simulate a website bookmark
-                let urls = ["https://shanehudson.net", "https://google.com", "https://twitter.com", "https://wikipedia.org"]
-                let randomIndex = Int(arc4random_uniform(UInt32(urls.count)))
-                let url = URL(string: urls[randomIndex])
-                print("adding url " + (url?.absoluteString)!)
-                
-                // Add a new anchor to the session
-                let anchor = URLAnchor(transform: transform)
-                anchor.url = url
-                sceneView.session.add(anchor: anchor)
-            }
+            let anchor = URLAnchor(transform: transform)
+            anchor.url = url
+            sceneView.session.add(anchor: anchor)
+
         }
     }
 }
